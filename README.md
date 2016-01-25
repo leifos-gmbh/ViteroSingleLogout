69cca905eb48c315668e5445ab681820506dc5e1
Fabian Wolf <wolf@leifos.com>
2016-01-25 12:19:51 +0100

    include files for 5.0.9

---
+++ README.md
@@ -0,0 +1,32 @@
+# Vitero
+
+Vitero  is a commercial conferencing tool. This plugin gives the possibility to logout users
+in a vitero virtual team room in ILIAS. You need the ILIAS vitero plugin to use vitero virtual team rooms in ILIAS.
+
+**Minimum ILIAS Version:**
+5.0.9
+
+**Maximum ILIAS Version:**
+5.0.999
+
+**Minimum Vitero Plugin Version:**
+1.5.1
+
+**Responsible Developer:**
+Stefan Meyer - meyer AT leifos.com
+
+**Supported Languages:**
+German, English
+
+### Quick Installation Guide
+1. Copy the content of this folder in <ILIAS_directory>/Customizing/global/plugins/Services/EventHandling/EventHook/ViteroSingleLogout
+
+2. Access to ILIAS and go to the administration page.
+
+3. Select "Plugins" in the menu on the right.
+
+5. Look for the ViteroSingleLogout plugin in the table and hit the "Action" button and select "Update".
+
+6. When ILIAS update the plugin, hit the "Action" button and select "Activate" that will appear instead of the "Update" link.
+
+7. Now, ILIAS will logout an user in vitero if this user logs out in ILIAS.
---
+++ classes/class.ilViteroSingleLogoutPlugin.php
@@ -0,0 +1,157 @@
+<?php
+/* Copyright (c) 1998-2009 ILIAS open source, Extended GPL, see docs/LICENSE */
+
+require_once 'Services/EventHandling/classes/class.ilEventHookPlugin.php';
+require_once 'Services/Component/classes/class.ilPluginAdmin.php';
+
+/**
+ * @author Fabian Wolf <wolf@leifos.com>
+ */
+class ilViteroSingleLogoutPlugin extends ilEventHookPlugin
+{
+	const VIT_CTYPE = 'Services';
+	const VIT_CNAME = 'Repository';
+	const VIT_SLOT_ID = 'robj';
+	const VIT_PNAME = 'Vitero';
+	const VIT_MIN_VERSION = '1.5.1';
+
+	/**
+	 * @var ilViteroPlugin
+	 */
+	protected $vitero = null;
+
+	/**
+	 * @return bool
+	 */
+	final public function getPluginName()
+	{
+		return "ViteroSingleLogout";
+	}
+
+	public function handleEvent($a_component, $a_event, $a_params)
+	{
+		global $ilUser;
+		switch($a_component)
+		{
+			case 'Services/Authentication':
+				switch($a_event)
+				{
+					case 'afterLogout':
+						if($this->initViteroPlugin())
+						{
+							if($vuser = $this->getViteroUserId($ilUser->getId()))
+							{
+								$sessions = $this->getUserSessions($vuser);
+
+								$GLOBALS['ilLog']->write(__METHOD__.': Deleting session codes: '.var_export($sessions, $vuser));
+
+								if(count($sessions) > 0)
+								{
+									$this->deleteSessions($sessions);
+								}
+							}
+						}
+					break;
+				}
+			break;
+		}
+
+		return true;
+	}
+
+	/**
+	 * @return bool
+	 */
+	protected function initViteroPlugin()
+	{
+		/**
+		 * @var ilPluginAdmin $ilPluginAdmin
+		 */
+		global $ilPluginAdmin;
+
+		if(!$ilPluginAdmin->isActive(self::VIT_CTYPE,self::VIT_CNAME, self::VIT_SLOT_ID,self::VIT_PNAME))
+		{
+			return false;
+		}
+
+		include_once './Services/Component/classes/class.ilPluginAdmin.php';
+
+		$vit = ilPluginAdmin::getPluginObject(
+			self::VIT_CTYPE,
+			self::VIT_CNAME,
+			self::VIT_SLOT_ID,
+			self::VIT_PNAME
+		);
+
+		if(ilComponent::isVersionGreaterString(self::VIT_MIN_VERSION, $vit->getVersion()))
+		{
+			return false;
+		}
+
+		$this->vitero = $vit;
+		return true;
+	}
+
+
+	/**
+	 * @return ilViteroPlugin|null
+	 */
+	protected function getViteroInstance()
+	{
+		if(!($this->vitero instanceof ilViteroPlugin))
+		{
+			$this->initViteroPlugin();
+		}
+		return $this->vitero;
+	}
+
+	/**
+	 * @param int $a_usr_id
+	 * @return bool|int
+	 */
+	protected function getViteroUserId($a_usr_id)
+	{
+		$this->getViteroInstance()->includeClass('class.ilViteroUserMapping.php');
+		$map = new ilViteroUserMapping();
+		$vuser = $map->getVUserId($a_usr_id);
+
+		return $vuser!= 0 ? $vuser : false;
+	}
+
+	/**
+	 * @param integer $a_vitero_user_id
+	 * @return string[]
+	 */
+	protected function getUserSessions($a_vitero_user_id)
+	{
+		$this->getViteroInstance()->includeClass('class.ilViteroSessionStore.php');
+
+		return  ilViteroSessionStore::getInstance()->getSessionsByUser($a_vitero_user_id);
+	}
+
+	protected function deleteSessions($a_sessions)
+	{
+
+		$this->getViteroInstance()->includeClass('class.ilViteroSessionCodeSoapConnector.php');
+
+		try
+		{
+			$connector = new ilViteroSessionCodeSoapConnector();
+			$connector->deleteSessionCodes($a_sessions);
+		}
+		catch(ilViteroConnectorException $e)
+		{
+			$GLOBALS['ilLog']->write(__METHOD__.': ViteroSingleLogout failed with message: '.$e->getMessage());
+		}
+	}
+
+	protected function beforeActivation()
+	{
+		if(!$this->initViteroPlugin())
+		{
+			ilUtil::sendFailure("You need atleast ILIAS Vitero Plugin version ". self::VIT_MIN_VERSION, true);
+			return false;
+		}
+		return true;
+	}
+}
