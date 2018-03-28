93b56f48eefd70e4eb655755bc27160bf5bbc334
smeyer-ilias <smeyer.ilias@gmx.de>
2018-03-28 11:45:28 +0200

    Increased version

--- plugin.php
+++ plugin.php
@@ -4,14 +4,14 @@
 $id = "xvitsl";
 
 // code version; must be changed for all code changes
-$version = "1.1.0";
+$version = "5.3.2.1";
 
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
-$ilias_min_version = "5.1.1";
-$ilias_max_version = "5.1.999";
+$ilias_min_version = "5.3.0";
+$ilias_max_version = "5.3.999";
 
 // optional, but useful: Add one or more responsible persons and a contact email
-$responsible = "Fabian Wolf";
-$responsible_mail = "wolf AT leifos.com";
+$responsible = "Stefan Meyer";
+$responsible_mail = "meyer AT leifos de";
 ?>
